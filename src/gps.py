e668d9cff36920f322e4643cfde4390e7e7586ba
kaave <abe@framelunch.jp>
2018-06-20 18:12:58 +0900

    gps: add get function

--- src/gps.py
+++ src/gps.py
@@ -1,10 +1,12 @@
 from serial import Serial
 from micropyGPS.micropyGPS import MicropyGPS
 from threading import Thread
-import time
+from time import sleep
+
+values = {'lat': 0, 'lng': 0, 'altitude': 0}
 
 
-def run_gps():
+def run_gps(gps):
     s = Serial('/dev/serial0', 9600, timeout=10)
     s.readline()  # drop first line
 
@@ -17,29 +19,51 @@ def run_gps():
             gps.update(x)
 
 
-def run_gps_thread():
-    gpsthread = Thread(target=run_gps, args=())
+def update_values(gps):
+    global values
+
+    if gps.clean_sentences <= 20:
+        return
+
+    values = {
+        'lat': gps.latitude[0],
+        'lng': gps.longitude[0],
+        'altitude': gps.altitude
+    }
+
+
+def start_update_values_loop(gps):
+    while True:
+        update_values(gps)
+        sleep(1.0)
+
+
+def run_gps_thread(gps):
+    gpsthread = Thread(target=run_gps, args=(gps, ))
     gpsthread.daemon = True
     gpsthread.start()
 
 
-if __name__ == '__main__':
+def run_start_update_values_loop_thread(gps):
+    gpsthread = Thread(target=start_update_values_loop, args=(gps, ))
+    gpsthread.daemon = True
+    gpsthread.start()
+
+
+def init():
     gps = MicropyGPS(9, 'dd')
-    run_gps_thread()
+
+    run_gps_thread(gps)
+    run_start_update_values_loop_thread(gps)
+
+
+def get():
+    return values
+
+
+if __name__ == '__main__':
+    init()
 
     while True:
-        if gps.clean_sentences > 20:
-            hour = gps.timestamp[0] % 24
-            minute = gps.timestamp[1]
-            second = gps.timestamp[2]
-
-            print('%2d:%02d:%04.1f' % (hour, minute, second))
-            print('緯度経度: %2.8f, %2.8f' % (gps.latitude[0], gps.longitude[0]))
-            print('海抜: %f' % gps.altitude)
-            print(gps.satellites_used)
-            print('衛星番号: (仰角, 方位角, SN比)')
-            for k, v in gps.satellite_data.items():
-                print('%d: %s' % (k, v))
-            print('')
-
-        time.sleep(1.0)
+        print(values)
+        sleep(2.0)
