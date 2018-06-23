ba6b2765245213c7c9f89ffc4448e9ffaa021335
kaave <junkjunctions@gmail.com>
2018-06-23 09:26:56 +0900

    fix: bme

--- src/bme280.py
+++ src/bme280.py
@@ -155,12 +155,13 @@ def get_data_bme280():
     bus = SMBus(bus_number)
     setup(i2c_address, bus)
 
-    digs = get_calib_param(i2c_address, bus)
-    data = []
-
     while True:
+        digs = get_calib_param(i2c_address, bus)
+        data = []
+
         for i in range(0xF7, 0xF7 + 8):
             data.append(bus.read_byte_data(i2c_address, i))
+
         pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
         temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
         hum_raw = (data[6] << 8) | data[7]
