bac9fabe548140722f7b54c1d7ba38079de69ab6
kaave <abe@framelunch.jp>
2018-07-08 21:53:43 +0900

    upload: 最終更新日が1日以前のファイルは削除

--- upload.py
+++ upload.py
@@ -1,7 +1,8 @@
-from dropbox import Dropbox
+from dropbox import Dropbox, files
 from dotenv import load_dotenv
 from glob import glob
-from os import path, environ
+from datetime import datetime
+from os import path, environ, remove
 
 load_dotenv()
 
@@ -10,9 +11,19 @@ token = environ['DROPBOX_TOKEN']
 if __name__ == '__main__':
     dbx = Dropbox(token)
     dbx.users_get_current_account()
-
+    now = datetime.now()
     csv_list = glob('csv/*.csv')
+
     for file_path in csv_list:
         with open(file_path, 'rb') as file:
-            dbx.files_upload(file.read(), f'/{path.basename(file_path)}')
-            print(f'uploaded: {file_path}')
+            dbx.files_upload(
+                file.read(),
+                f'/{path.basename(file_path)}',
+                mode=files.WriteMode.overwrite)
+            delta = now - datetime.fromtimestamp(path.getmtime(file_path))
+
+            if delta.days > 0:
+                remove(file_path)
+                print(f'uploaded and remove: {file_path}')
+            else:
+                print(f'uploaded: {file_path}')
